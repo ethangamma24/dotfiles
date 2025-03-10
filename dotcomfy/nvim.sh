5004af2bba3053f0b6d391fae92d1d4387bb0c21
Ethan Harmon <ethanharmon2018@gmail.com>
2025-03-09 21:14:06 -0400

    Updated nvim.sh

--- dotcomfy/nvim.sh
+++ dotcomfy/nvim.sh
@@ -2,3 +2,4 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> .zshrc
+echo 'Done with nvim.sh'
