1a60361abe505f953852a6f714eaa4cd9e3ed6de
Ethan Harmon <ethanharmon2018@gmail.com>
2025-03-15 21:21:12 -0400

    Testing reversion

--- dotcomfy/nvim.sh
+++ dotcomfy/nvim.sh
@@ -1,6 +1,12 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> .zshrc
+if [ -n "$BASH_VERSION" ]; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> .bashrc
+elif [ -n "$ZSH_VERSION" ]; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> .zshrc
+else
+    echo 'No idea what shell is on this system'
+fi
 export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 echo 'Done with nvim.sh'
