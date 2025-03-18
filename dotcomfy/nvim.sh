a988271f4cb926f60835510b5edf204114faa663
Ethan Harmon <ethanharmon2018@gmail.com>
2025-03-17 21:15:36 -0400

    Updated nvim.sh

--- dotcomfy/nvim.sh
+++ dotcomfy/nvim.sh
@@ -3,12 +3,12 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-if [ -n "$BASH_VERSION" ]; then
-    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.bashrc"
-    echo 'Adding path to .bashrc'
-elif [ -n "$ZSH_VERSION" ]; then
+if [ -n "$ZSH_VERSION" ]; then
     echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.zshrc"
     echo 'Adding path to .zshrc'
+elif [ -n "$BASH_VERSION" ]; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.bashrc"
+    echo 'Adding path to .bashrc'
 else
     echo 'No idea what shell is on this system'
 fi
