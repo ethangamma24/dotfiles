6ea6f83942af58d4e1624c10c71266d039d34acd
Ethan Harmon <ethanharmon2018@gmail.com>
2025-10-14 21:19:43 -0400

    Tweaks to tmux post_install_script

--- dotcomfy/tmux.sh
+++ dotcomfy/tmux.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-ls -al ~/.config/tmux/plugins/
-echo ~/.config/tmux/tmux.conf
+mkdir ~/.config/tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+tmux source ~/.config/tmux/tmux.conf
+echo -----------------------------------
+echo MAKE SURE TO INSTALL TMUX PLUGINS
+echo BY OPENING ~/.config/tmux/tmux.conf
+echo AND PRESSING `prefix + I`
+echo -----------------------------------
