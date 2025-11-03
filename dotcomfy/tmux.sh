d4d05ce7ff1b27bc90e30d255a7b640e5a6a19b4
Ethan Harmon <ethanharmon2018@gmail.com>
2025-11-03 17:30:22 -0500

    Additions to tmux post installation

--- dotcomfy/tmux.sh
+++ dotcomfy/tmux.sh
@@ -3,6 +3,8 @@
 mkdir ~/.config/tmux/plugins/
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 tmux source ~/.config/tmux/tmux.conf
+git clone https://github.com/tmux-plugins/tmux-continuum ~/.config/tmux/plugins/tmux-continuum
+git clone https://github.com/27medkamal/tmux-session-wizard ~/.config/tmux/plugins/tmux-session-wizard
 echo -----------------------------------
 echo MAKE SURE TO INSTALL TMUX PLUGINS
 echo BY OPENING ~/.config/tmux/tmux.conf
