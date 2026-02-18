0e887afd9f0be54a7e3359ae742f0ca41e7c3348
Ethan Harmon <ethanharmon2018@gmail.com>
2026-02-18 13:28:07 -0500

    Updated install scripts

--- dotcomfy/nvim.sh
+++ dotcomfy/nvim.sh
@@ -3,15 +3,18 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-# if [ -n "$ZSH_VERSION" ]; then
-#     echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.zshrc"
-#     echo 'Adding path to .zshrc'
-# elif [ -n "$BASH_VERSION" ]; then
-#     echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.bashrc"
-#     echo 'Adding path to .bashrc'
-# else
-#     echo 'No idea what shell is on this system'
-# fi
+if [ -n "$ZSH_VERSION" ]; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.zshrc"
+    echo 'Adding path to .zshrc'
+elif [ -n "$BASH_VERSION" ]; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.bashrc"
+    echo 'Adding path to .bashrc'
+elif [ -n "$env.NU_VERSION" ]; then
+    echo 'path add "/opt/nvim-linux-x86_64/bin"' >> "$HOME/.config/nushell/env.nu"
+    echo 'Adding path to .config/nushell/env.nu'
+else
+    echo 'No idea what shell is on this system'
+fi
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> "$HOME/.zshrc"
 export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 nvim --version
--- dotcomfy/tmux.sh
+++ dotcomfy/tmux.sh
@@ -5,8 +5,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 tmux source ~/.config/tmux/tmux.conf
 git clone https://github.com/tmux-plugins/tmux-continuum ~/.config/tmux/plugins/tmux-continuum
 git clone https://github.com/27medkamal/tmux-session-wizard ~/.config/tmux/plugins/tmux-session-wizard
-echo -----------------------------------
-echo MAKE SURE TO INSTALL TMUX PLUGINS
-echo BY OPENING ~/.config/tmux/tmux.conf
-echo AND PRESSING `prefix + I`
-echo -----------------------------------
+echo "-----------------------------------"
+echo "MAKE SURE TO INSTALL TMUX PLUGINS  "
+echo "BY OPENING ~/.config/tmux/tmux.conf"
+echo "AND PRESSING `prefix + I`          "
+echo "-----------------------------------"
