bc665458e4052e804c1d202101840e8a65b3aad8
Ethan Harmon <ethanharmon2018@gmail.com>
2026-05-07 16:14:21 -0400

    Update from unknown@endurance at 2026-05-07 16:14:21 EDT

--- nvim/lua/custom/plugins/vim-tmux-navigator.lua
+++ nvim/lua/custom/plugins/vim-tmux-navigator.lua
@@ -1,30 +1,15 @@
 return {
-  {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-      'TmuxNavigatorProcessList',
-    },
-    init = function()
-      -- Reusable function to register keymaps in different contexts
-      local function set_keymaps()
-        vim.keymap.set({ 'n', 't', 'v' }, '<C-h>', '<cmd>TmuxNavigateLeft<cr>')
-        vim.keymap.set({ 'n', 't', 'v' }, '<C-j>', '<cmd>TmuxNavigateDown<cr>')
-        vim.keymap.set({ 'n', 't', 'v' }, '<C-k>', '<cmd>TmuxNavigateUp<cr>')
-        vim.keymap.set({ 'n', 't', 'v' }, '<C-l>', '<cmd>TmuxNavigateRight<cr>')
-      end
-
-      -- Register once globally
-      set_keymaps()
-
-      -- Re-register for terminal buffers to prevent literal command injection
-      vim.api.nvim_create_autocmd('TermOpen', {
-        callback = set_keymaps,
-      })
-    end,
+  'christoomey/vim-tmux-navigator',
+  cmd = {
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+  },
+  keys = {
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-g>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
   },
 }
